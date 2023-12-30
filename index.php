217bf27c08ac19300381012cf2a5b8ac6ac7175b
Sagar Gurung <sgrgug@gmail.com>
2023-12-30 20:23:46 +0545

    version update

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 require_once __DIR__ . '/src/CurrencyConverter.php';
 
-use CurrencyConverter\CurrencyConverter;
+use Sgrgrg\CurrencyConverter\CurrencyConverter;
 
 echo CurrencyConverter::convert(3)
                         ->from('USD')
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,8 +1,8 @@
 <?php return array(
     'root' => array(
         'name' => 'sgrgrg/currency-converter',
-        'pretty_version' => '1.0.0',
-        'version' => '1.0.0.0',
+        'pretty_version' => '1.0.2',
+        'version' => '1.0.2.0',
         'reference' => NULL,
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
@@ -11,8 +11,8 @@
     ),
     'versions' => array(
         'sgrgrg/currency-converter' => array(
-            'pretty_version' => '1.0.0',
-            'version' => '1.0.0.0',
+            'pretty_version' => '1.0.2',
+            'version' => '1.0.2.0',
             'reference' => NULL,
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
