5cb4d00f0c56da0b259544cec9671294f45d5052
Sagar Gurung <sgrgug@gmail.com>
2024-01-01 16:50:21 +0545

    version change

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,8 +1,8 @@
 <?php return array(
     'root' => array(
         'name' => 'sgrgrg/currency-converter',
-        'pretty_version' => '1.0.2',
-        'version' => '1.0.2.0',
+        'pretty_version' => '1.1.0',
+        'version' => '1.1.0.0',
         'reference' => NULL,
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
@@ -282,8 +282,8 @@
             'dev_requirement' => true,
         ),
         'sgrgrg/currency-converter' => array(
-            'pretty_version' => '1.0.2',
-            'version' => '1.0.2.0',
+            'pretty_version' => '1.1.0',
+            'version' => '1.1.0.0',
             'reference' => NULL,
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
